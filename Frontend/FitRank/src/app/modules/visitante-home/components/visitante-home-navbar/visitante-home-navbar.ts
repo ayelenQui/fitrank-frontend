393bc18f104b65a11ed1c2de20fecc5e056b99b8
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-10-14 15:35:28 -0300

    Add routerLink to navbar buttons and update styles
    The 'Ingresar' and 'Registrarse' buttons in the visitante home navbar now use Angular's routerLink for navigation. Updated CSS to ensure navbar links have no underline and use the main text color. RouterLink was added to the component imports for proper functionality.

--- Frontend/FitRank/src/app/modules/visitante-home/components/visitante-home-navbar/visitante-home-navbar.ts
+++ Frontend/FitRank/src/app/modules/visitante-home/components/visitante-home-navbar/visitante-home-navbar.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-visitante-home-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './visitante-home-navbar.html',
   styleUrl: './visitante-home-navbar.css'
 })
