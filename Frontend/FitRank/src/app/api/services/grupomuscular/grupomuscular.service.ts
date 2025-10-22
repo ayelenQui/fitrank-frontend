d5949a48b9ff9e1ee3a52e3428afe439b2b1de1f
ayelenQui <145797580+ayelenQui@users.noreply.github.com>
2025-10-22 15:30:50 -0300

    merge de todo

--- Frontend/FitRank/src/app/api/services/grupomuscular/grupomuscular.service.ts
+++ Frontend/FitRank/src/app/api/services/grupomuscular/grupomuscular.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { GrupoMuscularDTO } from '../grupoMuscular/interfaces/grupomuscular.interface';
 
-export interface GrupoMuscularDTO {
-  id: number;
-  nombre: string;
-}
+ 
 
-@Injectable({ providedIn: 'root' })
-export class GrupoMuscularService {
+  @Injectable({ providedIn: 'root' })
+
+  export class GrupoMuscularService {
+
   private baseUrl = `${environment.apiUrl}/GrupoMuscular`;
 
   constructor(private http: HttpClient) { }
