ecb5579995bea62987e7768548030ea1223a7a44
ayelenQui <145797580+ayelenQui@users.noreply.github.com>
2025-11-28 20:14:55 -0300

    cambios de style

--- Frontend/FitRank/src/app/api/services/ingreso/ingreso.interface.ts
+++ Frontend/FitRank/src/app/api/services/ingreso/ingreso.interface.ts
@@ -1,18 +1,22 @@
+export interface UsuarioIngresoDTO {
+  id: number;
+  nombre: string;
+  apellido: string;
+  fotoDePerfil?: string | null;
+  cuotaPagadaHasta?: string | null;
+  email?: string | null;
+  telefono?: string | null;
+}
 
 export interface Ingreso {
   id: number;
   gimnasioId: number;
   usuarioId?: number;
-  usuario?: {
-    id: number;
-    nombre: string;
-    apellido: string;
-    fotoUrl?: string | null;
-    email?: string;
-  };
   monto: number;
   metodoPago: string;
   fecha: string;
-  observaciones?: string;
+  observaciones?: string | null;
   confirmado: boolean;
+
+  usuario?: UsuarioIngresoDTO | null;
 }
